bee69c7e7447b587f1aa1378c8563343aed39d05
goldstargreat <goldstargreat@gmail.com>
2026-04-07 11:55:33 +0900

    5장 도서상세정보, 카테고리별 도서 목록, 도서 카테고리와 출판사 모두 일치하는 필터링 결과 도서목록(Controller 완성)

--- src/main/java/kr/ac/kopo/gnuyog/bookmarket/controller/BookController.java
+++ src/main/java/kr/ac/kopo/gnuyog/bookmarket/controller/BookController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/books")
@@ -45,6 +47,14 @@ public class BookController {
     {
         List<Book> booksByCategory = bookService.getBookListBycategory(category);
         model.addAttribute("bookList", booksByCategory);
+        return "books";
+    }
+    @GetMapping("/filter/{bookFilter}")
+    public String requestBooksByFilter(
+            @MatrixVariable(pathVar = "bookFilter")Map<String, List<String>> bookFilter,
+            Model model) {
+        Set<Book> bookByFilter = bookService.getBookListByFilter(bookFilter);
+        model.addAttribute("bookList", bookByFilter);
         return "books";
     }
 }
