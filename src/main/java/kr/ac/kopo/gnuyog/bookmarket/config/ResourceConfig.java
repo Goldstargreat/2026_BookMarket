f5963ee0c3c846359c94ad9bbab9942bac5c5ac5
goldstargreat <goldstargreat@gmail.com>
2026-05-12 11:53:23 +0900

    7장 파일 업로드

--- src/main/java/kr/ac/kopo/gnuyog/bookmarket/config/ResourceConfig.java
+++ src/main/java/kr/ac/kopo/gnuyog/bookmarket/config/ResourceConfig.java
@@ -6,8 +6,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ResourceConfig implements WebMvcConfigurer
-{
+public class ResourceConfig implements WebMvcConfigurer {
     @Value("${file.uploadDir}")
     String fileDir;
 
@@ -15,7 +14,7 @@ public class ResourceConfig implements WebMvcConfigurer
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         WebMvcConfigurer.super.addResourceHandlers(registry);
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:///" + fileDir)
-                .setCachePeriod(60 * 60 * 24 * 365);
+                .addResourceLocations("file:///" + fileDir);
+//                .setCachePeriod(60 * 60 * 24 * 365);
     }
 }
