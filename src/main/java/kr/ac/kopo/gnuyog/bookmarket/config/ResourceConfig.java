d009ad48e7d6cf11d518e9a719295b3b05d423bb
goldstargreat <goldstargreat@gmail.com>
2026-05-19 08:38:52 +0900

    7장 파일 업로드(오류 수정)

--- src/main/java/kr/ac/kopo/gnuyog/bookmarket/config/ResourceConfig.java
+++ src/main/java/kr/ac/kopo/gnuyog/bookmarket/config/ResourceConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ResourceConfig implements WebMvcConfigurer {
-    @Value("${file.uploadDir}")
+    @Value("${file.uploadDir}") //
     String fileDir;
 
     @Override
