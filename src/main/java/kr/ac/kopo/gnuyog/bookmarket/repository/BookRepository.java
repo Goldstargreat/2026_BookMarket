2bb630c202bcc5b668cba05cde5f8b918725018a
goldstargreat <goldstargreat@gmail.com>
2026-06-09 08:53:51 +0900

    BookRepository.java 주석 추가

--- src/main/java/kr/ac/kopo/gnuyog/bookmarket/repository/BookRepository.java
+++ src/main/java/kr/ac/kopo/gnuyog/bookmarket/repository/BookRepository.java
@@ -1,4 +1,5 @@
 package kr.ac.kopo.gnuyog.bookmarket.repository;
+// "책에 대한 데이터 저장/조회 담당" 계층
 
 import kr.ac.kopo.gnuyog.bookmarket.domain.Book;
 import java.util.List;
@@ -7,9 +8,9 @@ import java.util.Set;
 
 public interface BookRepository
 {
-  List<Book> getAllBookList(); // 모든 책 목록 반환
-  Book getBookById(String bookId); // 특정 ID의 책 1권 조회
+  List<Book> getAllBookList(); // 모든 책 목록 반환하는 메서드 선언
+  Book getBookById(String bookId); // 특정 ID의 책 1권 조회하는 메서드 선언
   List<Book> getBookListByCategory(String category); // "IT", "소설" 등의 카테고리로 필터링
   Set<Book> getBookListByFilter(Map<String, List<String>> filter); // 여러 조건으로 검색(고급 검색 키- 값 구조)
-  void setNewBook(Book book);
+  void setNewBook(Book book); // 새 책을 추가하는 메서드 선언. 반환값 없음.
 }
