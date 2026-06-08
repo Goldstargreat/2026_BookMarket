186b3572b0b0ef58e4dcbede69222e688eb0daa3
goldstargreat <goldstargreat@gmail.com>
2026-06-09 08:51:53 +0900

    Book.java 주석 추가

--- src/main/java/kr/ac/kopo/gnuyog/bookmarket/domain/Book.java
+++ src/main/java/kr/ac/kopo/gnuyog/bookmarket/domain/Book.java
@@ -14,16 +14,24 @@ import java.math.BigDecimal;
 @Getter
 
 public class Book
+    //  책을 정의함. 책에 대한 정보를 담는 틀.
 {
-    @BookId
-    @Pattern(regexp = "isbn[0-9a-zA-Z]+", message = "{Pattern.book.bookId}")
+    @BookId // 중복 체크 어노테이션
+    @Pattern(regexp = "isbn[0-9a-zA-Z] + ", message = "{Pattern.book.bookId}")
+    // 오류 메시지는 messages.properties 파일에서 가져온다
     private String bookId; // 도서 id
+
     @Size(min = 4, max = 50, message = "{Size.book.name}")
+    // 책 제목은 4자 이상 50자 이하여야 함. 위반 시 오류 메시지 표시.
     private String name; // 도서 제목
+
     @Min(value = 0, message = "{Min.book.unitPrice}")
+    // 가격은 0 이상이어야 해 (음수 불가)
     @Digits(integer = 8, fraction = 2, message = "{Digits.book.unitPirce}")
+    // 정수 8자리, 소수 2자리까지 허용
     @NotNull(message = "{NotNull.book.unitPrice}")
     private BigDecimal unitPrice; // 단가
+
     private String author; // 저자
     private String description; // 설명
     private String publisher; // 출판사
@@ -31,8 +39,10 @@ public class Book
     private long unitsInStock; // 재고 수량
     private String releaseDate; // 출판일
     private String condition; // 신규도서 or 중고도서 or 전자책(도서 상태)
-    private String fileName; // 도서 이미지 파일 이름
-    private MultipartFile bookImage; // 도서 이미지 파일 객체
+    // 저자, 설명, 출판사, 분류, 재고량, 출판일, 상태. 검증 어노테이션이 없으므로 아무 값이나 들어와도 됨.
+
+    private String fileName; // 업로드된 도서 이미지 파일의 이름 저장
+    private MultipartFile bookImage; // 실제로 업로드 된 도서 이미지 파일 객체를 담는다. 서버에 저장한다.
 }
 
 // @Data, @Getter, @Setter를 넣으면 Book 클래스의 모든 멤버 변수의 Setter()와 Getter()메서드가 추가됨
