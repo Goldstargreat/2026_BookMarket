f7e7b893b468d8603ed9df17816d5a9a2f9de3da
goldstargreat <goldstargreat@gmail.com>
2026-09-15 12:19:07 +0900

    13장 장바구니 구현 - cartItem 삭제 기능까지 구현

--- src/main/resources/static/js/controller.js
+++ src/main/resources/static/js/controller.js
@@ -1,12 +1,17 @@
 function addToCart(bookId) {
-    if(confirm("장바구니에 해당 도서를 추가하시겠습니까?")){
+    if(confirm("장바구니에 해당 도서를 추가하시겠습니까?"))
+    {
         document.addForm.action = "/BookMarket/cart/book/" + bookId;
         document.addForm.submit();
     }
 }
 
-function removeFromCart(bookId, cartId) {
-    document.removeForm.action = "/BookMarket/cart/book/" + bookId;
-    document.removeForm.submit();
-    setTimeout('location.reload()', 10);
+function removeFromCart(bookId)
+{
+    if (confirm("장바구니에서 해당 도서를 삭제하시겠습니까?") == true)
+    {
+        document.removeForm.action = "/BookMarket/cart/book/" + bookId;  // /cart/remove/ → /cart/book/ 로 수정
+        document.removeForm.submit();
+        setTimeout('location.reload()', 10);
+    }
 }
